750e08bf6c66d4887d8708e205c3a0596e19f378
xiaofengming <519734050@qq.com>
2016-11-01 09:58:15 +0800

    Toast一句话

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -1,11 +1,10 @@
 package com.example.administrator.myapplication;
 
-import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
