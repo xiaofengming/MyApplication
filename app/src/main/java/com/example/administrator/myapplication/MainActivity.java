706848f2cb13cbe2e81c702591c581413bd95441
xiaofengming <519734050@qq.com>
2016-11-05 12:33:57 +0800

    add a line

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(MainActivity.this,"xiaofengming!",Toast.LENGTH_SHORT).show();
+        Toast.makeText(MainActivity.this,"hello world!",Toast.LENGTH_SHORT).show();
     }
 }
